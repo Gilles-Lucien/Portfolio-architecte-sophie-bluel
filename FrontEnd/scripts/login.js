6822107e8badd031066e1a95411715bcf8c91c86
Gilles Lucien <gpeltier2@gmail.com>
2023-12-29 16:27:01 +0100

    Refactor login function and add error handling

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,38 +1,34 @@
-function loginUser() {
-    const emailInput = document.querySelector('input[type="email"]');
-    const passwordInput = document.querySelector('input[type="password"]');
-    
-    const email = emailInput.value;
-    const password = passwordInput.value;
-    
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        const token = data.token;
-        // Store the token in localStorage
-        localStorage.setItem('token', token);
-        // Do something with the token
-        console.log(token);
-    })
-    .catch(error => {
-        // Handle error
-        console.error(error);
-    });
+// Here is the code to login a user
 
-    if (token !== undefined) {
-        window.location.href = "http://localhost:5678/admin.html";
-    }
+function loginUser() {
+  const emailInput = document.querySelector('input[type="email"]');
+  const passwordInput = document.querySelector('input[type="password"]');
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const token = data.token;
+      // Store the token in localStorage
+      localStorage.setItem("token", token);
+      // Redirect to index.html if token is defined
+      token && (window.location.href = "./index.html");
+      // If token is undefined, alert the user
+      token || alert("Invalid email or password");
+    });
 }
 
-const form = document.querySelector('form');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    loginUser();
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  loginUser();
 });
