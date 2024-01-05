1c0dc3ae85a16152aa9b6fd785f4d7a4f912d869
Gilles Lucien <gpeltier2@gmail.com>
2024-01-05 11:18:09 +0100

    Add logout functionality and update edit.js

--- FrontEnd/scripts/edit.js
+++ FrontEnd/scripts/edit.js
@@ -1,5 +1,11 @@
 // Here is the code to display the edit mode and to edit the works
 
+// tâches réalisées depuis la dernière session :
+// - création du bouton logout, nettoyage du localstorage on click
+
+// tâches à réaliser :
+// supprimer les catégories lors du chargement en mode adimn
+
 // function to handle the click on a edit button
 function handleEditButtonClick(event) {
   // Handle the click event for the edit button
@@ -21,6 +27,15 @@ function checkToken() {
       editButton.classList.remove("hidden");
       editButton.addEventListener("click", handleEditButtonClick);
     });
+
+    // Change innerText of logout button to login button and clear local storage on click
+    const logoutButton = document.querySelector(".logout");
+    logoutButton.innerHTML = `<a href="#" class="logout">logout</a>`;
+    logoutButton.addEventListener("click", () => {
+      localStorage.clear();
+      checkToken();
+    });
+
   } else {
     // Toggle on the .hidden class for elements with .editBar class
     const editBars = document.querySelectorAll(".editBar");
@@ -33,10 +48,11 @@ function checkToken() {
     editButtons.forEach((editButton) => {
       editButton.classList.add("hidden");
     });
+
+    // Change innerText of logout button to login button and clear local storage on click
+    const logoutButton = document.querySelector(".logout");
+    logoutButton.innerHTML = `<a href="./login.html" class="logout">login</a>`;
   }
 }
 
-export {
-    checkToken,
-
-}
+export { checkToken };
