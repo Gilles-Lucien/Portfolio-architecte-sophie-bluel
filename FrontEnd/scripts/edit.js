100c13e35f5089d7e8bd06212c9efd3f9449eb3a
Gilles Lucien <gpeltier2@gmail.com>
2024-01-09 15:37:36 +0100

    Add event listener for modal's background and fetch categories for modal select

--- FrontEnd/scripts/edit.js
+++ FrontEnd/scripts/edit.js
@@ -16,11 +16,13 @@ import {
 } from "./work.js";
 
 
-// Create an event listener on the modal's close buttons
+// Create an event listener on the modal's close buttons and the modal's background
 const closeButtons = document.querySelectorAll(".close");
 closeButtons.forEach((closeButton) => {
   closeButton.addEventListener("click", handleCloseButtonClick);
 });
+const modalBackground = document.querySelector(".modal__background");
+modalBackground.addEventListener("click", handleCloseButtonClick);
 
 // Create an event listener on the modal's left arrow
 const leftArrow = document.querySelector(".fa-arrow-left");
@@ -32,6 +34,7 @@ function handleEditButtonClick() {
   const modalBackground = document.querySelector(".modal__background");
   modal.classList.remove("hidden");
   modalBackground.classList.remove("hidden");
+  
 }
 
 // function to handle the click on the close button
@@ -186,5 +189,64 @@ function handleWorkDeletion(figure) {
     });
 }
 
+// function to fetch the categories and display them in the modal <select>
+async function getCategoriesModal() {
+  try {
+    const response = await fetch("http://localhost:5678/api/categories");
+    const categories = await response.json();
 
-export { checkToken, handleEditButtonClick, getWorksModal };
+    const modal__select = document.querySelector("#category");
+    modal__select.innerHTML = "";
+
+    categories.forEach((category) => {
+      const option = document.createElement("option");
+      option.value = category.id;
+      option.innerText = category.name;
+      modal__select.appendChild(option);
+    });
+    console.log(categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+  }
+}
+
+// function to display the img loaded from the input file into the <img class="preview">
+function displayImage(input) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const preview = document.querySelector("#preview");
+      preview.src = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+    // toggle off and on hidden class for elements that must appear or disappear once the picture is loaded
+    // vider le texte de .addPictureLabel
+    //retirer hidden de #preview
+    //ajouter hidder à .fa-picture
+    // ajouter hidden au p 
+
+    const addPictureLabel = document.querySelector(".addPictureLabel");
+    addPictureLabel.classList.add("hidden");
+    const preview = document.querySelector("#preview");
+    preview.classList.remove("hidden");
+    const faPicture = document.querySelector(".fa-picture");
+    faPicture.classList.add("hidden");
+    const p = document.querySelector(".addPicture p");
+    p.classList.add("hidden");
+
+  }
+}
+
+// Call the displayImage function when a file is loaded via #addPictureInput
+const addPictureInput = document.querySelector("#addPictureInput");
+addPictureInput.addEventListener("change", function() {
+  displayImage(this);
+});
+
+
+
+
+export { checkToken, handleEditButtonClick, getWorksModal, getCategoriesModal };
